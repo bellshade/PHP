ff04550de7b0abe39df220e3504efae739cff6ac
Ikramullah <ikramullahzx@gmail.com>
2021-10-01 21:26:47 +0800

    refactor: Tambah file untuk logical operator docs: Revisi dokumentasi

---
+++ basics/3_operator/4_logical_operator.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Operator - Comparison Operator
+ * Cara menggunakan operator pembanding di PHP
+ * Penjelasannya silahkan lihat dalam README.
+ *
+ * Pro tips: Eksekusi kode ini di console sehingga hasilnya akan terlihat
+ */
+
+$a = 3;
+$b = 5;
