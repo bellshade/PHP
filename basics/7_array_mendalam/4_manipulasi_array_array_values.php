72495f4e4dfc0fc903e4e4f13aa5b130a5916f9e
Reza Rachmanuddin <rezarahmanudin@gmail.com>
2021-10-12 23:22:44 +0700

    add: array_keys dan array_values

---
+++ basics/7_array_mendalam/4_manipulasi_array_array_values.php
@@ -0,0 +1,13 @@
+<?php
+
+$cars = [
+    'japan' => 'toyota',
+    'germany' => 'bmw',
+];
+
+echo '<pre>';
+print_r($cars);
+echo '</pre>';
+
+echo 'array_values($cars) : ';
+print_r(array_values($cars));
