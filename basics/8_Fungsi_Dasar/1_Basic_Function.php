8c6652e9ba157400d1277cc938c6c74a0a7a3335
M Haikal <muhammad.haikal@dipstrategy.com>
2021-10-20 19:26:51 +0700

    Add : Nambah Materi baru FUngsi dasar : Parameter Return

--- basics/8_Fungsi_Dasar/1_Basic_Function.php
+++ basics/8_Fungsi_Dasar/1_Basic_Function.php
@@ -9,20 +9,3 @@ function hello()
 //Sintaks Call Function
 hello();
 //============================================
-
-
-// echo "<br><h3>4. Function Rekursif</h3>";
-// //============================================4. Function Rekursif============================================
-// //Sebuah Function yang melakukan pemanggilan terhadap diri sendiri
-// function rekursif(int $angka)
-// {
-//     if ($angka > 0) {
-//         echo "<br>$angka";
-//         rekursif($angka - 1);
-//     } else {
-//         return "$angka";
-//     }
-// }
-// //Sintaks Call Function
-// echo rekursif(10);
-// //============================================
