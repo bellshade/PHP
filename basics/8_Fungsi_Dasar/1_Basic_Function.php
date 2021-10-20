b8b643c8441e06cde00ba32ee658993bccb877d1
M Haikal Anatama <muhamadhaikal25@gmail.com>
2021-10-20 18:29:11 +0700

    add:Menambah Materi baru di Basic, Pengenalan Fungsi

---
+++ basics/8_Fungsi_Dasar/1_Basic_Function.php
@@ -0,0 +1,28 @@
+<?php
+
+echo "<h1>1. Pengenalan Function Dasar</h1>";
+//============================================1.Teknik Dasar============================================
+function hello()
+{
+    echo "Hello World";
+}
+//Sintaks Call Function
+hello();
+//============================================
+
+
+// echo "<br><h3>4. Function Rekursif</h3>";
+// //============================================4. Function Rekursif============================================
+// //Sebuah Function yang melakukan pemanggilan terhadap diri sendiri
+// function rekursif(int $angka)
+// {
+//     if ($angka > 0) {
+//         echo "<br>$angka";
+//         rekursif($angka - 1);
+//     } else {
+//         return "$angka";
+//     }
+// }
+// //Sintaks Call Function
+// echo rekursif(10);
+// //============================================
