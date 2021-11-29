824b5e565bbde7b88b9ef5497e95e6ba56ee5afe
Muhammad Azqilana <mazqulunu@gmail.com>
2021-11-29 15:54:15 +0800

    fix:  Memperbaiki contoh

--- basics/6_manipulasi_string/6_strip_slashes.php
+++ basics/6_manipulasi_string/6_strip_slashes.php
@@ -7,16 +7,19 @@ echo "<hr>";
 
 echo "<h3>Penggunaan stripslashes</h3>";
 // ---------------
-echo "Sebelum menggunakan stripslashes(): ",PHP_EOL;
+echo "Sebelum menggunakan stripslashes(): ", PHP_EOL;
 $text = "Ayo belajar \ di bellshade";
-echo "$text",PHP_EOL;
-
+echo $text, PHP_EOL;
+// Output
+// Ayo belajar \ di bellshade
 echo "<br>";
 //-----------------
-echo "Sesudah menggunakan stripslashes(): ",PHP_EOL;
-$text = "Sandi: \"Hai, apa kabar semuanya?\"";
+echo "Sesudah menggunakan stripslashes(): ", PHP_EOL;
+$text = "Sandi:\"Hai, apa kabar semuanya?\"";
 $text1 = stripslashes($text);
-echo "$text1";
+echo $text1, PHP_EOL;
+// Output
+// Sandi:"Hai, apa kabar semuanya?"
 
 echo "<hr>";
 
