135d9e6bb924293d1c9bbb327ba3969c3095c649
raifandi <92188764+KrOxYs@users.noreply.github.com>
2021-12-08 13:06:44 +0700

    update: memperbaiki materi
    silahkan di cek :)

--- basics/14_oop_dasar/2_class_and_object.php
+++ basics/14_oop_dasar/2_class_and_object.php
@@ -1,29 +1,29 @@
 <?php
 
-echo '<pre>';
+echo "<pre>";
 
-echo '<h1>Membuat class di PHP</h1>';
+echo "<h1>Membuat class di PHP</h1>";
 
-echo 'class Kelas {
+echo "class Kelas {
 
-}';
+}";
 class Kelas
 {
 
 }
-echo '<br>';
+echo "<br>";
 
-echo 'Membuat instance dari class',PHP_EOL;
-echo '<br>';
-echo 'namaVariable = new namaClassNya();',PHP_EOL;
-echo 'namaVariable2 = new namaClassNya();';
+echo "Membuat instance dari class",PHP_EOL;
+echo "<br>";
+echo "namaVariable = new namaClassNya();",PHP_EOL;
+echo "namaVariable2 = new namaClassNya();";
 
-echo '<br>';
+echo "<br>";
 $kelas1 = new Kelas();
 $kelas2 = new Kelas();
-echo 'Hasilnya Jika Di var_dump()',PHP_EOL;
-echo '<br>';
+echo "Hasilnya Jika Di var_dump()",PHP_EOL;
+echo "<br>";
 var_dump($kelas1);
 var_dump($kelas2);
 
-echo '</pre>';
+echo "</pre>";
