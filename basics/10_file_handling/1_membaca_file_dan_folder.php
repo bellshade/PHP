0d900d57dfc157c2b4db26d796fc1074cc024696
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-02-22 14:10:39 +0700

    add: Menambahkan Membaca File dan Folder

---
+++ basics/10_file_handling/1_membaca_file_dan_folder.php
@@ -0,0 +1,40 @@
+<?php
+
+echo "<pre>";
+
+echo "<h1>Membaca File PHP</h1>";
+
+echo "Pertama buat variabel yang mengarahkan ke lokasi file berada
+
+// Letak filenya
+file = '../../assets/content/basics/10_file_handling/file_handling.txt';
+
+// Membuka file yang ingin dibaca
+open = fopen(file, 'r');
+
+// Mencari size dari filenya
+size = filesize(file);
+
+// Untuk membaca sebuah file maka gunakan lah fread
+filedata = fread(open, size);
+
+print_r(filedata);
+
+<b>Hasil print yang akan di dapatkan adalah sebagai berikut:</b>
+
+";
+
+// Letak filenya
+$file = '../../assets/content/basics/10_file_handling/file_handling.txt';
+
+// Membuka file yang ingin dibaca
+$open = fopen($file, 'r');
+
+// Mencari size dari filenya
+$size = filesize($file);
+
+// Untuk membaca sebuah file maka gunakan lah fread
+$filedata = fread($open, $size);
+
+print_r($filedata);
+?>
