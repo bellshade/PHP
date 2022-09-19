ebedf23511e80ddaac9cf83e146b8dcf6896c3d1
Mushlih Almubarak <mushlih_31@mushlih.cf>
2022-09-19 17:45:53 +0300

    fix: memperbaiki penamaan dari "reCHAPTCHA" ke "reCAPTCHA" (https://github.com/bellshade/PHP/pull/174)

--- utilities/reCAPTCHA/google_reCAPTCHA/captcha.php
+++ utilities/reCAPTCHA/google_reCAPTCHA/captcha.php
@@ -9,7 +9,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Membuat Google reCHAPTCHA</title>
+        <title>Membuat Google reCAPTCHA</title>
     </head>
     <body>
         
