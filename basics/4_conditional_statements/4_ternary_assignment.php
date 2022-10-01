e4a2c3774bfc5ce0642c1fab69981d6b9c6c2de4
Malvin Valerian <78136971+malvinval@users.noreply.github.com>
2022-10-01 14:38:04 +0700

    Update 4_ternary_assignment.php

--- basics/4_conditional_statements/4_ternary_assignment.php
+++ basics/4_conditional_statements/4_ternary_assignment.php
@@ -1,10 +1,22 @@
 <?php
-    $nilai_ujian = 86;
+    
+/**
+ * Filename: 4_ternary_assignment.php
+ *
+ * File ini berisi tentang contoh penggunaan Operator Ternary pada PHP.
+ * Untuk penjelasan mengenai Switch,
+ * kamu bisa kunjungi link berikut ini:
+ * https://github.com/bellshade/PHP/tree/main/basics/4_conditional_statements
+ *
+ * Silahkan gunakan browser favorit kamu untuk melihat file ini.
+ * Jangan lupa juga untuk menghidupkan web server yang kamu gunakan ya.
+ */
 
-    // Mengisi nilai true ke variabel $isLulus apabila kondisi ($nilai_ujian > 75) menghasilkan true
-    $isLulus = $nilai_ujian > 75 ? true : false;
+$nilai_ujian = 86;
 
-    // Mencetak string "Lulus KKM !" jika $isLulus bernilai true
-    // Mencetak string "Tidak lulus KKM !" jika $isLulus tidak bernilai true
-    echo $isLulus ? "Lulus KKM !" : "Tidak lulus KKM !";
-?>
+// Mengisi nilai true ke variabel $isLulus apabila kondisi ($nilai_ujian > 75) menghasilkan true
+$isLulus = $nilai_ujian > 75 ? true : false;
+
+// Mencetak string "Lulus KKM !" jika $isLulus bernilai true
+// Mencetak string "Tidak lulus KKM !" jika $isLulus tidak bernilai true
+echo $isLulus ? "Lulus KKM !" : "Tidak lulus KKM !";
