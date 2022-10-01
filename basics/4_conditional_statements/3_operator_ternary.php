ceb170f08896f10ec005f32f5a90b8744773f855
Malvin Valerian <78136971+malvinval@users.noreply.github.com>
2022-10-01 14:37:12 +0700

    Update 3_operator_ternary.php

--- basics/4_conditional_statements/3_operator_ternary.php
+++ basics/4_conditional_statements/3_operator_ternary.php
@@ -1,5 +1,19 @@
 <?php
-    $nilai_tugas = 86;
-    // Jika value $nilai_tugas lebih dari 75, cetak string "Lulus KKM !"
-    // Jika value $nilai_tugas tidak lebih dari 75, cetak string "Tidak lulus KKM !"
-    echo $nilai_tugas > 75 ? "Lulus KKM !" : "Tidak lulus KKM !";
+
+/**
+ * Filename: 3_operator_ternary.php
+ *
+ * File ini berisi tentang contoh penggunaan Operator Ternary pada PHP.
+ * Untuk penjelasan mengenai Switch,
+ * kamu bisa kunjungi link berikut ini:
+ * https://github.com/bellshade/PHP/tree/main/basics/4_conditional_statements
+ *
+ * Silahkan gunakan browser favorit kamu untuk melihat file ini.
+ * Jangan lupa juga untuk menghidupkan web server yang kamu gunakan ya.
+ */
+
+$nilai_tugas = 86;
+
+// Jika value $nilai_tugas lebih dari 75, cetak string "Lulus KKM !"
+// Jika value $nilai_tugas tidak lebih dari 75, cetak string "Tidak lulus KKM !"
+echo $nilai_tugas > 75 ? "Lulus KKM !" : "Tidak lulus KKM !";
