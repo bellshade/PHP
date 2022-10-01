af7761c8809f8efd6c73e514d21b21c09d24562b
Malvin Valerian <78136971+malvinval@users.noreply.github.com>
2022-10-01 14:19:36 +0700

    Update 3_operator_ternary.php

--- basics/4_conditional_statements/3_operator_ternary.php
+++ basics/4_conditional_statements/3_operator_ternary.php
@@ -3,4 +3,3 @@
     // Jika value $nilai_tugas lebih dari 75, cetak string "Lulus KKM !"
     // Jika value $nilai_tugas tidak lebih dari 75, cetak string "Tidak lulus KKM !"
     echo $nilai_tugas > 75 ? "Lulus KKM !" : "Tidak lulus KKM !";
-?>
