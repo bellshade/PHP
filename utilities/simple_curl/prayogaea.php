403c6bea938b691dcaaa0b6478a1a9f20da39f91
prayogaea <prayogaekaardiansyah@gmail.com>
2022-10-27 11:02:27 +0700

    docs: utilities/simple_curl/

--- utilities/simple_curl/prayogaea.php
+++ utilities/simple_curl/prayogaea.php
@@ -1,9 +1,10 @@
-<?php 
-function https_request($url){
+<?php
 
+function http_request($url)
+{
     $data = curl_init();
     curl_setopt($data, CURLOPT_URL, $url);
-    curl_setopt($data,CURLOPT_USERAGENT,'Chrome');
+    curl_setopt($data, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
     curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
     $output = curl_exec($data);
     curl_close($data);
@@ -11,7 +12,7 @@ function https_request($url){
     return $output;
 }
 
-$data_user = https_request("https://api.github.com/users/prayogaea");
+$data_user = http_request("https://api.github.com/users/prayogaea");
 $data_user = json_decode($data_user, true);
 
 // echo "<pre>";
