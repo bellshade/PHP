bc24571f628177d9b9b77f4b4bb91c3d83710bda
prayogaea <prayogaekaardiansyah@gmail.com>
2022-10-27 10:32:19 +0700

    docs: utilities/simple_curl/

--- utilities/simple_curl/curl.php
+++ utilities/simple_curl/curl.php
@@ -3,6 +3,7 @@ function https_request($url){
 
     $data = curl_init();
     curl_setopt($data, CURLOPT_URL, $url);
+    curl_setopt($data,CURLOPT_USERAGENT,'Chrome');
     curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
     $output = curl_exec($data);
     curl_close($data);
