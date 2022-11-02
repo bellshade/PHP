2b41a95dd36891b1828fb5794edbe0ddf4e1a580
prayogaea <prayogaekaardiansyah@gmail.com>
2022-11-02 13:42:46 +0700

    docs: utilities/fpdf/fpdf.php

--- utilities/fpdf/fpdf.php
+++ utilities/fpdf/fpdf.php
@@ -1,11 +1,8 @@
-<?php 
+<?php
 
 require 'src/fpdf.php';
 
 $pdf = new FPDF();
 $pdf->AddPage();
-$pdf->SetFont('Arial','B',20);
-$pdf->Cell(40,10,'Hello World!');
+$pdf->Cell('Hello World!');
 $pdf->Output();
-
-?>
