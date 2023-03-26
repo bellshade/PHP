4eee46a3ccc82112697b226c606f4a10bc02fded
Zain Adam <zainadam.id@gmail.com>
2023-03-27 05:59:11 +0700

    add: menambahkan bab 14.7 setter dan getter

---
+++ basics/14_oop_dasar/7_setter_dan_getter2.php
@@ -0,0 +1,58 @@
+<?php
+
+// Mendefinisikan kelas Pengguna
+class Pengguna
+{
+    public $tanggalLahir;
+    public $usia;
+
+    // Setter untuk properti tanggalLahir
+    // sekaligus menghitung dan mengeset properti usia
+    public function setTanggalLahir($tanggalLahir)
+    {
+        // Mengeset properti tanggalLahir
+        $this->tanggalLahir = $tanggalLahir;
+
+        // Mengakses nilai properti tanggalLahir untuk menghitung usia
+        $dateTimeTanggalLahir = new DateTime($this->tanggalLahir);
+        $dateTimeTanggalHariIni = new DateTime();
+        $hasilHitungUsia = $dateTimeTanggalHariIni->diff($dateTimeTanggalLahir)->y;
+
+        // Mengeset properti usia dengan nilai dari variabel $hasilHitungUsia
+        $this->usia = $hasilHitungUsia;
+    }
+
+    // Getter untuk properti tanggalLahir
+    public function getTanggalLahir()
+    {
+        // Mengakses nilai properti tanggalLahir untuk DIKEMBALIKAN
+        // dalam format penulisan tanggal Indonesia
+        $dateTimeTanggalLahir = new DateTime($this->tanggalLahir);
+        $formatterTanggal = new IntlDateFormatter('id_ID', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
+        return $formatterTanggal->format($dateTimeTanggalLahir);
+    }
+
+    // Getter untuk properti usia
+    public function getUsia()
+    {
+        // Mengakses nilai properti usia untuk diformat dan DIKEMBALIKAN
+        return "{$this->usia} tahun";
+    }
+}
+
+// Instansiasi objek
+$pengguna = new Pengguna();
+
+// Mengeset nilai properti tanggalLahir melalui setter
+$pengguna->setTanggalLahir('2000-01-01');
+
+// Mengakses nilai properti tanggalLahir
+// yang telah diformat melalui getter-nya untuk ditampilkan
+echo $pengguna->getTanggalLahir(); // 1 Januari 2000
+
+// Menampilkan baris baru
+echo PHP_EOL;
+
+// Mengakses nilai properti usia
+// yang telah diformat melalui getter-nya untuk ditampilkan
+echo $pengguna->getUsia(); // 23 tahun (2023)
---
+++ basics/14_oop_dasar/7_setter_dan_getter6.php
@@ -0,0 +1,80 @@
+<?php
+
+class Pengguna
+{
+    private $tanggalLahir;
+
+    static private function apakahFormatTanggalLahirSalah($tanggalLahir)
+    {
+        if (preg_match('/^\d{4}-\d{2}-\d{2}$/', $tanggalLahir)) {
+            return false;
+        }
+        
+        return true;
+    }
+
+    private function setTanggalLahir($tanggalLahir)
+    {
+        if (self::apakahFormatTanggalLahirSalah($tanggalLahir)) {
+            echo '### Format tanggal salah ###';
+            return;
+        }
+
+        $this->tanggalLahir = new DateTime($tanggalLahir);
+    }
+
+    private function getTanggalLahir()
+    {
+        $formatterTanggal = new IntlDateFormatter('id_ID', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
+        return $formatterTanggal->format($this->tanggalLahir);
+    }
+
+    private function getUsia()
+    {
+        $tanggalSekarang = new DateTime();
+        $hasilHitungUsia = $tanggalSekarang->diff($this->tanggalLahir)->y;
+
+        return "{$hasilHitungUsia} tahun";
+    }
+
+    public function __set($name, $value)
+    {
+        if ($name == 'tanggalLahir') {
+            $this->setTanggalLahir($value);
+
+            return;
+        }
+
+        echo '### Properti tidak ditemukan ###';
+    }
+
+    public function __get($name)
+    {
+        if ($name == 'tanggalLahir') {
+            return $this->getTanggalLahir();
+        }
+
+        if ($name == 'usia') {
+            return $this->getUsia();
+        }
+
+        echo '### Properti tidak ditemukan ###';
+    }
+}
+
+// Instansiasi objek
+$pengguna = new Pengguna();
+
+// Mengeset properti tanggalLahir melalui setter ajaib
+$pengguna->tanggalLahir = '2000-01-01';
+
+// Mengakses nilai properti tanggalLahir
+// yang telah diformat melalui getter ajaib untuk ditampilkan
+echo $pengguna->tanggalLahir; // 1 Januari 2000
+
+// Mencetak baris baru
+echo PHP_EOL;
+
+// Mengakses nilai properti usia
+// yang telah diformat melalui getter ajaib untuk ditampilkan
+echo $pengguna->usia; // 23 tahun (2023)
