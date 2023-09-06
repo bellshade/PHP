76c8876369de464eb65752fc03dd84bad78f8cda
kvnrynfl <kevinreynaufal2004@gmail.com>
2023-09-06 17:55:57 +0700

    style: memperbaiki format kode dan menambahkan komen

--- basics/15_oop_lanjutan/5_abstract_class.php
+++ basics/15_oop_lanjutan/5_abstract_class.php
@@ -39,7 +39,7 @@ class FrontendDeveloper extends Pengguna
     // Metode khusus untuk FrontendDeveloper
     public function apaPekerjaanFrontendDeveloper()
     {
-        echo 'Saya bekerja untuk mengembangkan antarmuka pengguna.' .PHP_EOL;
+        echo 'Saya bekerja untuk mengembangkan antarmuka pengguna.' . PHP_EOL;
     }
 }
 
@@ -49,7 +49,7 @@ class BackendDeveloper extends Pengguna
     // Implementasi printPerkenalan untuk BackendDeveloper
     public function printPerkenalan()
     {
-        echo 'Saya adalah seorang Backend Developer dengan nama ' . $this->getNama() . '.'.PHP_EOL;
+        echo 'Saya adalah seorang Backend Developer dengan nama ' . $this->getNama() . '.' . PHP_EOL;
     }
 
     // Metode khusus untuk BackendDeveloper
@@ -59,79 +59,8 @@ class BackendDeveloper extends Pengguna
     }
 }
 
-echo "
-// Abstract class Pengguna
-abstract class Pengguna
-{
-    // Properti untuk menyimpan nama pengguna.
-    protected \$nama;
-
-    // Konstruktor yang mengatur nilai nama pada saat objek dibuat.
-    public function __construct(\$nama)
-    {
-        \$this->nama = \$nama;
-    }
-
-    // Metode abstrak printPerkenalan yang harus diimplementasikan oleh kelas turunan.
-    abstract public function printPerkenalan();
-
-    // Metode lainnya untuk mengambil nilai nama.
-    public function getNama()
-    {
-        return \$this->nama;
-    }
-}
-
-// Kelas turunan FrontendDeveloper
-class FrontendDeveloper extends Pengguna
-{
-    // Implementasi printPerkenalan untuk FrontendDeveloper
-    public function printPerkenalan()
-    {
-        echo 'Saya adalah seorang Frontend Developer dengan nama ' . \$this->getNama() . '.' . PHP_EOL;
-    }
-
-    // Metode khusus untuk FrontendDeveloper
-    public function apaPekerjaanFrontendDeveloper()
-    {
-        echo 'Saya bekerja untuk mengembangkan antarmuka pengguna.' .PHP_EOL;
-    }
-}
-
-// Kelas turunan BackendDeveloper
-class BackendDeveloper extends Pengguna
-{
-    // Implementasi printPerkenalan untuk BackendDeveloper
-    public function printPerkenalan()
-    {
-        echo 'Saya adalah seorang Backend Developer dengan nama ' . \$this->getNama() . '.'.PHP_EOL;
-    }
-
-    // Metode khusus untuk BackendDeveloper
-    public function apaPekerjaanBackendDeveloper()
-    {
-        echo 'Saya bekerja untuk mengatur logika dan server.' . PHP_EOL;
-    }
-}
-";
-
-echo "
-// Menginstansiasi objek FrontendDeveloper
-\$frontendDeveloper = new FrontendDeveloper('John');
-// Memanggil metode printPerkenalan untuk FrontendDeveloper.
-\$frontendDeveloper->printPerkenalan();
-// Memanggil metode apaPekerjaanFrontendDeveloper.
-\$frontendDeveloper->apaPekerjaanFrontendDeveloper();
-
-// Menginstansiasi objek BackendDeveloper
-\$backendDeveloper = new BackendDeveloper('Alice');
-// Memanggil metode printPerkenalan untuk BackendDeveloper.
-\$backendDeveloper->printPerkenalan();
-// Memanggil metode apaPekerjaanBackendDeveloper.
-\$backendDeveloper->apaPekerjaanBackendDeveloper();
-";
-
-echo "<br><br><h3>Hasil Dari Instansiasi Objek FrontendDeveloper</h3>";
+// Tidak bisa menginstansiasi object pada abstrack class, jika di lakukan maka akan menghasilkan error
+// $pengguna = new Pengguna("Mail"); 
 
 // Menginstansiasi objek FrontendDeveloper
 $frontendDeveloper = new FrontendDeveloper('Upin');
